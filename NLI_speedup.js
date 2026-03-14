92da39d74bb90e5826f76b51f77c41173fc87211
MathisSinet <mathis.sinet@gmail.com>
2026-03-14 18:56:30 +0100

    NLI Speedup: adjusted equation UI

--- NLI_speedup.js
+++ NLI_speedup.js
@@ -1477,7 +1477,7 @@ var getQRepr = () => {
 var getPrimaryEquation = () => {
     let result = ``;
 
-    theory.primaryEquationHeight = 90
+    theory.primaryEquationHeight = 65
     theory.primaryEquationScale = 1.25
 
     const q = getQRepr();
@@ -1507,8 +1507,8 @@ var getSecondaryEquation = () => {
         return result;
     }
 
-    theory.secondaryEquationHeight = 100;
-    theory.secondaryEquationScale = 1.25;
+    theory.secondaryEquationHeight = 90;
+    theory.secondaryEquationScale = 1.05;
 
     let k = "{a_1}x + a_0";
     if (kTermPerma.level > 0) {
